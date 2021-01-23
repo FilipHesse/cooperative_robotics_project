24787a7e525de448e791f02803313ff89859cd1d
Filip Hesse <Filip_Hesse@yahoo.de>
2021-01-23 11:47:28 +0100

    Compute altitude of uvms (uvms.alt)

--- cooperative_robotics/ComputeTaskReferences.m
+++ cooperative_robotics/ComputeTaskReferences.m
@@ -15,10 +15,17 @@ uvms.xdot.v.ang = Saturate(0.2 * ang, 0.2);
 uvms.xdot.v.lin = Saturate(0.2 * lin, 0.2);
 
 %% reference for horizontal attitude
-uvms.xdot.ha = - 0.2 * norm(uvms.rho); 
+uvms.xdot.ha = - 0.2 * norm(uvms.rho);
 
-%% reference for altitude
-uvms.xdot.min_alt = - 0.5 * (uvms.sensorDistance- (uvms.min_alt+0.5));
+% TASK 1.2 --------------------------------------
+%% Compute altitude from sensor distance: Transform sensor distance from 
+% vehicle frame to world frame and consider only the z-component
+% => The distance will be projected on the k-axis of the world 
+uvms.alt = - [0 0 1]*uvms.wTv(1:3,1:3)*[0; 0; -uvms.sensorDistance];
+
+%% reference for minimal altitude
+velocity_min_alt = 1;        %Move faster than normal navigation as this is more relevant
+uvms.xdot.min_alt = Saturate(velocity_min_alt * (uvms.min_alt + uvms.target_distance_above_min_alt - uvms.alt), 1);
 
 
 
